bffee8878d3a221dc3da2811caaeb3d33deaf5b8
Diego Bizzotto <diegobizzotto2003@gmail.com>
2023-09-21 22:45:56 -0300

    Refactor: Definida estrutura da BottomNavigationBar

--- lib/pages/layout/layout_view.dart
+++ lib/pages/layout/layout_view.dart
@@ -20,33 +20,59 @@ class _LayoutViewState extends State<LayoutView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: widget.controller.state.selectedIndex,
-        children: widget.controller.state.pages,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        unselectedIconTheme: const IconThemeData(color: Colors.white),
-        unselectedItemColor: Colors.white,
-        selectedIconTheme: IconThemeData(color: Theme.of(context).colorScheme.secondaryContainer),
-        selectedItemColor: Theme.of(context).colorScheme.secondaryContainer,
-        backgroundColor: Theme.of(context).colorScheme.tertiary,
-        currentIndex: widget.controller.state.selectedIndex,
-        onTap: (int index) {
-          setState(() {
-            widget.controller.onItemTapped(index);
-          });
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.people_alt_outlined),
-            label: 'Usuários',
+        body: Stack(children: [
+          IndexedStack(
+            index: widget.controller.state.selectedIndex,
+            children: widget.controller.state.pages,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outlined),
-            label: 'Perfil',
+        ]),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: widget.controller.state.selectedIndex == 0 ? FloatingActionButton(
+          shape: const CircleBorder(),
+          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+          child: const Icon(
+            Icons.add_outlined,
+            color: Colors.blueGrey,
           ),
-        ],
-      ),
-    );
+          onPressed: () {},
+        ) : null,
+        bottomNavigationBar: Row(
+          children: [
+            Expanded(
+              child: BottomAppBar(
+                height: 64,
+                shape: const CircularNotchedRectangle(),
+                color: Theme.of(context).colorScheme.tertiary,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    IconButton(
+                      padding: const EdgeInsets.only(left: 64, right: 64),
+                      color: widget.controller.state.selectedIndex == 0 ? Theme.of(context).colorScheme.secondaryContainer : Colors.white,
+                      icon: const Icon(Icons.people_alt_outlined),
+                      onPressed: () {
+                        setState(() {
+                          widget.controller.onItemTapped(0);
+                        });
+                      },
+                    ),
+                    Container(width: 64),
+                    IconButton(
+                      padding: const EdgeInsets.only(left: 64, right: 64),
+                      color: widget.controller.state.selectedIndex == 1 ? Theme.of(context).colorScheme.secondaryContainer : Colors.white,
+                      icon: const Icon(Icons.person_outlined),
+                      onPressed: () {
+                        setState(() {
+                          widget.controller.onItemTapped(1);
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
