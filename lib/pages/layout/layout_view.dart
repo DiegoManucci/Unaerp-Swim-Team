fda9de9edf0d5310828594fa99a047618b309a34
Diego Bizzotto <diegobizzotto2003@gmail.com>
2023-09-26 23:10:23 -0300

    Refactor: Mudança visual da BottomNavigationBar e remoção do FAB

--- lib/pages/layout/layout_view.dart
+++ lib/pages/layout/layout_view.dart
@@ -26,51 +26,26 @@ class _LayoutViewState extends State<LayoutView> {
             children: widget.controller.state.pages,
           ),
         ]),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: widget.controller.state.selectedIndex == 0 ? FloatingActionButton(
-          shape: const CircleBorder(),
-          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-          child: const Icon(
-            Icons.add_outlined,
-            color: Colors.blueGrey,
-          ),
-          onPressed: () {},
-        ) : null,
-        bottomNavigationBar: Row(
-          children: [
-            Expanded(
-              child: BottomAppBar(
-                height: 64,
-                shape: const CircularNotchedRectangle(),
-                color: Theme.of(context).colorScheme.tertiary,
-                child: Row(
-                  mainAxisSize: MainAxisSize.max,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    IconButton(
-                      padding: const EdgeInsets.only(left: 64, right: 64),
-                      color: widget.controller.state.selectedIndex == 0 ? Theme.of(context).colorScheme.secondaryContainer : Colors.white,
-                      icon: const Icon(Icons.people_alt_outlined),
-                      onPressed: () {
-                        setState(() {
-                          widget.controller.onItemTapped(0);
-                        });
-                      },
-                    ),
-                    Container(width: 64),
-                    IconButton(
-                      padding: const EdgeInsets.only(left: 64, right: 64),
-                      color: widget.controller.state.selectedIndex == 1 ? Theme.of(context).colorScheme.secondaryContainer : Colors.white,
-                      icon: const Icon(Icons.person_outlined),
-                      onPressed: () {
-                        setState(() {
-                          widget.controller.onItemTapped(1);
-                        });
-                      },
-                    ),
-                  ],
-                ),
-              ),
+        bottomNavigationBar: BottomNavigationBar(
+          unselectedIconTheme: const IconThemeData(color: Colors.white),
+          unselectedItemColor: Colors.white,
+          selectedIconTheme: IconThemeData(color: Theme.of(context).colorScheme.secondaryContainer),
+          selectedItemColor: Theme.of(context).colorScheme.secondaryContainer,
+          backgroundColor: Theme.of(context).colorScheme.tertiary,
+          currentIndex: widget.controller.state.selectedIndex,
+          onTap: (int index) {
+            setState(() {
+              widget.controller.onItemTapped(index);
+            });
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_alt_outlined),
+              label: 'Usuários',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outlined),
+              label: 'Perfil',
             ),
           ],
         ));
