90d730e413644572a4aee4e1326848260b9e08d0
Diego Bizzotto <diegobizzotto2003@gmail.com>
2023-09-26 22:45:23 -0300

    Refactor: Alterado tipo de usuario de String para UserType

--- lib/types/user.dart
+++ lib/types/user.dart
@@ -1,15 +1,17 @@
+import 'package:unaerp_swim_team/types/user_type.dart';
+
 class User {
   String _name;
   String _email;
-  String _type;
+  UserType _type;
 
   User(this._name, this._email, this._type);
 
   String get name => _name;
   String get email => _email;
-  String get type => _type;
+  UserType get type => _type;
 
   set name(String name) => _name = name;
   set email(String email) => _email = email;
-  set type(String type) => _type = type;
+  set type(UserType type) => _type = type;
 }
---
+++ lib/types/user_type.dart
@@ -0,0 +1,5 @@
+enum UserType {
+  administrador,
+  treinador,
+  atleta,
+}
