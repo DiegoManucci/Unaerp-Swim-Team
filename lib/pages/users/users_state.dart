90b7f3c9a3b1c3aeee6fbd4ba6a57fe3dc796244
Diego Bizzotto <diegobizzotto2003@gmail.com>
2023-09-28 23:04:27 -0300

    Refactor: Tornando atributo privado

--- lib/pages/users/users_state.dart
+++ lib/pages/users/users_state.dart
@@ -2,7 +2,7 @@ import 'package:unaerp_swim_team/types/user.dart';
 import 'package:unaerp_swim_team/types/user_type.dart';
 
 class UsersState {
-  List<User> users = [
+  List<User> _users = [
     User('Administrador 1', 'admin1@example.com', UserType.administrador),
     User('Atleta 1', 'atleta1@example.com', UserType.atleta),
     User('Treinador 1', 'treinador1@example.com', UserType.treinador),
@@ -16,4 +16,7 @@ class UsersState {
     User('Atleta 4', 'atleta2@example.com', UserType.atleta),
     User('Treinador 4', 'treinador2@example.com', UserType.treinador),
   ];
+
+  List<User> get users => _users;
+  set users(List<User> users) => _users = users;
 }
