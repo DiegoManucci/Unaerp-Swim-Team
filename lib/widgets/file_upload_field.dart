fc286002473f28170cc93609d93a79b45f4b6420
Diego Bizzotto <diegobizzotto2003@gmail.com>
2023-10-12 12:11:21 -0300

    Feat: Criada componente genérico para upload de arquivos

---
+++ lib/widgets/file_upload_field.dart
@@ -0,0 +1,47 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class FileUploadField extends StatelessWidget {
+  final String documentType;
+  final String filePath;
+  final Function(String)? onFileSelected;
+
+  const FileUploadField({
+    super.key,
+    required this.documentType,
+    required this.filePath,
+    this.onFileSelected,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Upload $documentType:',
+          style: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(height: 8.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(filePath.isEmpty ? 'Nenhum arquivo selecionado' : filePath),
+            ElevatedButton(
+              onPressed: () async {
+                FilePickerResult? result =
+                await FilePicker.platform.pickFiles();
+
+                if (result != null) {
+                  onFileSelected?.call(result.files.single.path ?? '');
+                }
+              },
+              child: const Text('Selecionar'),
+            ),
+          ],
+        ),
+        const Divider(),
+      ],
+    );
+  }
+}
