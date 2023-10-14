d43fe9b6c50b422a1a1b4a36a155e377a1de3781
Diego Bizzotto <diegobizzotto2003@gmail.com>
2023-10-14 11:08:45 -0300

    Refactor: Adicionada senha para a classe de usuário

--- lib/types/user.dart
+++ lib/types/user.dart
@@ -3,15 +3,18 @@ import 'package:unaerp_swim_team/types/user_type.dart';
 class User {
   String _name;
   String _email;
+  String _password;
   UserType _type;
 
-  User(this._name, this._email, this._type);
+  User(this._name, this._email, this._password, this._type);
 
   String get name => _name;
   String get email => _email;
+  String get password => _password;
   UserType get type => _type;
 
   set name(String name) => _name = name;
   set email(String email) => _email = email;
+  set password(String password) => _password = password;
   set type(UserType type) => _type = type;
 }
